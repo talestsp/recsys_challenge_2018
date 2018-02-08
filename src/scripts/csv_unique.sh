96d6fedea6761620c857fd9e7cfa07eaf78940fa
Tales Pimentel <tales.tsp@gmail.com>
2018-02-07 21:53:11 -0300

    Unique tracks generator now is fixed. It wont generate counter column and wont miss the header

--- src/scripts/csv_unique.sh
+++ src/scripts/csv_unique.sh
@@ -8,5 +8,9 @@ FILEPATH2=$2
 echo $FILEPATH1
 echo $FILEPATH2
 
-sort $FILEPATH1 | uniq -c > $FILEPATH2
+header=$( head -1 $FILEPATH1 )
+echo $header
 
+echo "$header" > $FILEPATH2
+
+tail -n +2 $FILEPATH1 | sort | uniq >> $FILEPATH2
