39f571126de1ce2491c1d7022c0e6ab19e8b88c5
Tatiana <tatiana.silva@ccc.ufcg.edu.br>
2018-02-09 14:14:27 -0300

    Solve problem with semi-coma

--- src/scripts/to_csv.py
+++ src/scripts/to_csv.py
@@ -26,33 +26,31 @@ p_cols = ["name", "collaborative", "pid", "modified_at", "num_tracks", "num_albu
 t_cols = ["artist_name", "track_uri", "artist_uri", "track_name", "album_uri", "duration_ms", "album_name"]
 p_t_cols = ["pid", "track_uri", "pos"]
 try:
-    with open(output_data_dir + "playlists.csv", "w", encoding = 'utf-8') as p_out_file:
-        p_file = csv.writer(p_out_file, quotechar='', quoting=csv.QUOTE_NONE , lineterminator = '\n', delimiter=';', escapechar='\\')
-        p_file.writerow(p_cols)
-        with open(output_data_dir + "tracks.csv", "w", encoding = 'utf-8') as t_out_file:
-            t_file = csv.writer(t_out_file, quotechar='', quoting=csv.QUOTE_NONE , lineterminator = '\n', delimiter=';', escapechar='\\')
-            t_file.writerow(t_cols)
-            with open(output_data_dir + "play_track.csv", "w", encoding = 'utf-8') as p_t_out_file:
-                p_t_file = csv.writer(p_t_out_file, quotechar='', quoting=csv.QUOTE_NONE , lineterminator = '\n', delimiter=';', escapechar='\\')
-                p_t_file.writerow(p_t_cols)
-                for filename in os.listdir(input_data_dir):
-                    if('.json' in filename):
-                        filepath = input_data_dir + filename
-                        print(filepath)
-                        data = json.load(open(filepath))
-                        playlists = data["playlists"]
-                        for p in playlists:
-                            line = [p["name"], p["collaborative"], p["pid"], p["modified_at"], p["num_tracks"], p["num_albums"], p["num_followers"]]
-                            p_file.writerow(line)
-                            pid = p["pid"]
-                            tracks = p["tracks"]
-                            for t in tracks:
-                                line = [t["artist_name"], t["track_uri"], t["artist_uri"], t["track_name"], t["album_uri"], t["duration_ms"], t["album_name"]]
-                                t_file.writerow(line)
-                                track_uri = t["track_uri"]
-                                pos = t["pos"]
-                                line = [pid, track_uri, pos]
-                                p_t_file.writerow(line)
+	with open("playlists.csv", "w", encoding = 'utf-8') as p_out_file:
+	    p_file = csv.writer(p_out_file, quoting=csv.QUOTE_MINIMAL , lineterminator = '\n', delimiter=';', escapechar='\\')
+	    p_file.writerow(p_cols)
+	    with open("tracks.csv", "w", encoding = 'utf-8') as t_out_file:
+		    t_file = csv.writer(t_out_file, quoting=csv.QUOTE_MINIMAL , lineterminator = '\n', delimiter=';', escapechar='\\')
+		    t_file.writerow(t_cols)
+		    with open("play_track.csv", "w", encoding = 'utf-8') as p_t_out_file:
+			    p_t_file = csv.writer(p_t_out_file, quoting=csv.QUOTE_MINIMAL , lineterminator = '\n', delimiter=';', escapechar='\\')
+			    p_t_file.writerow(p_t_cols)
+			    for x in os.listdir('.'):
+			    	if('.json' in x):
+				    	data = json.load(open(x))
+				    	playlists = data["playlists"]
+				    	for p in playlists:
+				    		line = [p["name"], p["collaborative"], p["pid"], p["modified_at"], p["num_tracks"], p["num_albums"], p["num_followers"]]
+				    		p_file.writerow(line)
+				    		pid = p["pid"]
+				    		tracks = p["tracks"]
+				    		for t in tracks:
+				    			line = [t["artist_name"], t["track_uri"], t["artist_uri"], t["track_name"], t["album_uri"], t["duration_ms"], t["album_name"]]	
+				    			t_file.writerow(line)
+				    			track_uri = t["track_uri"]
+				    			pos = t["pos"]
+				    			line = [pid, track_uri, pos]
+				    			p_t_file.writerow(line)
 except Exception as e:
-    print(line)
-    pass
+	print(e)
+	pass
