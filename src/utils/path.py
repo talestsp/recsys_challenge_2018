f9aa784786f335c65f039abf88e298019573599b
Tales Pimentel <tales.tsp@gmail.com>
2018-02-27 01:19:21 -0300

    Isolating util function to a separate module

--- src/utils/path.py
+++ src/utils/path.py
@@ -1,7 +1,9 @@
 import os
 
-def fix_path(path):
+def fix_path(path, is_dir=True):
     base_path = os.getcwd()
     path = os.path.join(base_path, path)
-    path = os.path.abspath(os.path.realpath(path)) + "/"
+    path = os.path.abspath(os.path.realpath(path))
+    if is_dir:
+        path = path + "/"
     return path
