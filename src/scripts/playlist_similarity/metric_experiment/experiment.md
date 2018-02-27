14eb91a7d7b9f27b11a2375fb1eed61c2074d4b5
Tales Tenorio de Souza Pimentel <talestsp@users.noreply.github.com>
2018-02-27 17:42:06 -0300

    Similarity metric eval (#11)
    * Script to generate a flat table with play_track.csv. I removes song position data. This new data is lighter and good for playlist_similarity script
    
    * Fixing csv separator
    
    * Script to generate for each playlits the mos similar playlists using jaccard metric.
    
    * Script to generate for each playlits the mos similar playlists using jaccard metric FIXING 1.
    
    * similar_playlists.py running with play_track.csv. Flat dataset usage appeared to produce higher execution times
    
    * Removing flatten_play_track.py due to worse exection time
    
    * Script to run on parallel cpus the similar_playlist code
    
    * Stable version of multi cpu for similarity generation
    
    * Stable version of playlist similarity generator
    
    * Updating gitignore
    
    * Script to generate playlist similarities (using jaccard)
    
    * Script to generate playlist similarities (using jaccard)
    
    * Similarity config added to gitignore
    
    * Isolating util function to a separate module
    
    * Isolating util function to a separate module
    
    * Now pids are selected not only by a range, but as a list of pids
    
    * updating gitignore
    
    * Now pids are selected not only by a range, but as a list of pids
    
    * updating gitigore to avoid config fiels
    
    * updating gitigore to avoid config fiels
    
    * Inserting helping comments
    
    * Code to generate playlist similarities optimized! Now three times faster (at least)
    
    * Config files example. They are configurations to run playlist similarities.
    
    * updating gitignore
    
    * Code to generate playlist similarities optimized! Now three times faster (at least)
    
    * initial code for evaluating which similarity metric to use: jaccard or percentual_intersec

---
+++ src/scripts/playlist_similarity/metric_experiment/experiment.md
@@ -0,0 +1,10 @@
+# Similarity Metric Experiment
+This experiment aims to verify if there is evidence that both similarity metrics 
+(jaccard and percentual_intersec) ranks equally the similar playlists.
+
+## Subset pids
+Randomly picked pids can be found at 
+**percentual_intersec_config.json** and **jaccard_config.json**
+ 
+## Evaluation
+Check for both similarities rankings.
--- src/scripts/playlist_similarity/metric_experiment/experiment.sh
+++ src/scripts/playlist_similarity/metric_experiment/experiment.sh
@@ -1,1 +1,4 @@
 #!/usr/bin/env bash
+
+PYTHONPATH=.:src ~/anaconda3/bin/python src/scripts/playlist_similarity/similar_playlists_multi_cpu.py /home/tales.tsp/dev/recsys_challenge_2018/src/scripts/playlist_similarity/metric_experiment/jaccard_config.json
+PYTHONPATH=.:src ~/anaconda3/bin/python src/scripts/playlist_similarity/similar_playlists_multi_cpu.py /home/tales.tsp/dev/recsys_challenge_2018/src/scripts/playlist_similarity/metric_experiment/percentual_intersec_config.json
