a69d8da896d5eb052ac4bb742091e162955e9184
kaiokassiano <kaiokmo@lsd.ufcg.edu.br>
2018-03-01 23:26:15 -0300

    Remove hardcoded header on the output.csv file
    Now we get the header from the last modified csv among them all.

--- src/scripts/playlist_similarity/append_output.sh
+++ src/scripts/playlist_similarity/append_output.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 output_csv=/root/recsys/recsys_env/output.csv
+output_path=/root/recsys/recsys_env/output/
+
+echo $(head -n 1 $output_path$(ls -1t | head -n 1)) >> $output_csv
 echo "percentual_intersec;pid;similar_pid" >> $output_csv
- 
+
 for file in $(ls ./output/); do
-	path="/root/recsys/recsys_env/output/"$file
+	path=$output_path$file
 	while IFS= read -r line; do
 		if [ ${line:0:1} != "p" ]; then
 			#echo $line
